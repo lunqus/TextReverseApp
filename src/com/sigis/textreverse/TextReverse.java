b8bdc98e370d791af4f4c6c80e645a5de32290ad
LUNKUS <sigitas@lunkus.com>
2020-05-24 09:10:27 +0300

    Created frame, buttons and labels

--- src/com/sigis/textreverse/TextReverse.java
+++ src/com/sigis/textreverse/TextReverse.java
@@ -2,13 +2,62 @@ package com.sigis.textreverse;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class TextReverse {
+public class TextReverse implements ActionListener {
+
+    JTextField mTextField;
+    JButton reverseButton;
+    JLabel promtLabel;
+    JLabel contentLabel;
 
     public TextReverse() {
 
+        JFrame mFrame = new JFrame("Enter Text");
+
+        // Set layout
+        mFrame.setLayout(new FlowLayout());
+
+        // Give the Frame a size
+        mFrame.setSize(250, 250);
+
+        // Set close operation
+        mFrame.setDefaultCloseOperation(mFrame.EXIT_ON_CLOSE);
+
+        // Create Text Field
+        mTextField = new JTextField(10);
+
+        // Create Action Command
+        mTextField.setActionCommand("textField");
+
+        // Crate Buttons
+        reverseButton = new JButton("Reverse");
+
+        // Add Action Listeners
+        mTextField.addActionListener(this);
+        reverseButton.addActionListener(this);
+
+        // Create prompt Labels
+        promtLabel = new JLabel("Enter text: ");
+        contentLabel = new JLabel("");
+
+        // Add components to mField
+        mFrame.add(promtLabel);
+        mFrame.add(mTextField);
+        mFrame.add(reverseButton);
+        mFrame.add(contentLabel);
+
+        // Set visible
+        mFrame.setVisible(true);
+
+
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 
     public static void main(String[] args) {
 
@@ -16,8 +65,9 @@ public class TextReverse {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                new TextField();
+                new TextReverse();
             }
         });
     }
+
 }
